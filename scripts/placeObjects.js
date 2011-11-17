596d69b7b86b34ebde7d7c79e3c9c3b49c997042
Benjamin Sangster <benjamin.e.sangster@gmail.com>
2011-11-17 12:53:56 -0800

    Removed a few unused plugins. Fixes to placeObject.

--- scripts/placeObjects.js
+++ scripts/placeObjects.js
@@ -55,37 +55,48 @@ getVoteTime = function (voteHandle) {
 
 addLink = function (voteHandle, objectHandle) {
 
-   var currTime
-     , nextTime
-     , objData = objectList[objectHandle]
-     , idx
+   var currentTime
+     , newTime
+     , oldHandle
      ;
 
-   if (voteList[voteHandle]) {
+   if (voteList[voteHandle] && !voteList[objectHandle]) {
 
-      if (!objData) { objectList[objectHandle] = []; objData = objectList[objectHandle]; }
+      voteList[voteHandle].links.push(objectHandle);
+      currentTime = getVoteTime(objectList[objectHandle]);
+      newTime = getVoteTime(voteHandle);
+      oldHandle = objectList[objectHandle];
+      if (!oldHandle || (newTime && (!currentTime || (newTime < currentTime)))) {
 
-      if (objData.length) {
+         updateVoteObjectPositions(oldHandle);
+         objectList[objectHandle] = voteHandle;
+         updateVoteObjectPositions(objectList[objectHandle]);
+      }
+   }
+};
 
-         currTime = getVoteTime(objData[0]);
-         nextTime = getVoteTime(voteHandle);
-         if (nextTime && (nextTime < currTime)) {
+removeLink = function (voteHandle, objectHandle) {
 
-            idx = voteList[objData[0]].links.indexOf(objectHandle);
-            if (idx !== -1) { voteList[objData[0]].links.splice(idx, 1); }
-            voteList[voteHandle].links.push(objectHandle);
+   var idx
+     , links
+     ;
+   if (voteList[voteHandle] && !voteList[objectHandle]) {
+
+      idx = voteList[voteHandle].links.indexOf(objectHandle);
+      if (idx !== -1) { voteList[voteHandle].links.splice(idx, 1); }
+      if (objectList[objectHandle] === voteHandle) {
+
+         links = (dmz.object.subLinks(objectHandle, dmz.mind.CanvasLink) || []);
+         links = links.concat(dmz.object.superLinks(objectHandle, dmz.mind.CanvasLink) || []);
+         links = links.filter(function (objectHandle) { return voteList[objectHandle]; });
+         links.sort(function (vote1, vote2) { return getVoteTime(vote1) - getVoteTime(vote2); });
+         objectList[objectHandle] = links[0];
+         if (objectList[objectHandle]) {
+
+            updateVoteObjectPositions(voteHandle);
+            updateVoteObjectPositions(objectList[objectHandle]);
          }
       }
-      else { voteList[voteHandle].links.push(objectHandle); }
-
-      if (objectList[objectHandle].indexOf(voteHandle) === -1) {
-
-         objectList[objectHandle].push(voteHandle);
-      }
-      objectList[objectHandle].sort(function (vote1, vote2) {
-
-         return getVoteTime(vote1) - getVoteTime(vote2);
-      });
    }
 };
 
@@ -94,45 +105,17 @@ function (linkObjHandle, attrHandle, superHandle, subHandle) {
 
    var superType = dmz.object.type(superHandle)
      , subType = dmz.object.type(subHandle)
-     , currTime
-     , nextTime
      ;
 
    if (superType && subType) {
 
       if (superType.isOfType(dmz.mind.ToolLinkType) ||
          subType.isOfType(dmz.mind.ToolLinkType)) {}
-      else if (voteList[superHandle] && voteList[subHandle]) {} // Don't move votes when connected votes move
+      else if (voteList[superHandle] && voteList[subHandle]) {}
       else if (voteList[superHandle]) { addLink(superHandle, subHandle); }
       else if (voteList[subHandle]) { addLink(subHandle, superHandle); }
    }
 });
-
-removeLink = function (voteHandle, objectHandle) {
-
-   var item = voteList[voteHandle]
-     , idx
-     ;
-
-   if (item && !voteList[objectHandle]) {
-
-      idx = item.links.indexOf(objectHandle);
-      if (idx !== -1) {
-
-         item.links.splice(idx, 1);
-         idx = objectList[objectHandle].indexOf(voteHandle);
-         if (idx !== -1) {
-
-            objectList[objectHandle].splice(idx, 1);
-            if ((idx === 0) && objectList[objectHandle].length) {
-
-               item = voteList[objectList[objectHandle][0]];
-               if (item) { item.links.push(objectHandle); }
-            }
-         }
-      }
-   }
-};
 
 dmz.object.unlink.observe(self, dmz.mind.CanvasLink,
 function (linkObjHandle, attrHandle, superHandle, subHandle) {
@@ -148,19 +131,22 @@ getPosition = function (index, length) {
    return dmz.vector.create(Math.cos(angle), 0, Math.sin(angle)).multiply(OBJECT_RADIUS);
 };
 
-updateVoteObjectPositions = function (voteHandle, attr) {
+updateVoteObjectPositions = function (voteHandle) {
 
-   var data = voteList[voteHandle];
+   var data = voteList[voteHandle]
+     , list
+     ;
    if (data && data.position && data.links) {
 
-      data.links.sort(function (obj1, obj2) {
+      list = data.links.filter(function (obj1) { return (objectList[obj1] === voteHandle); });
+      list.sort(function (obj1, obj2) {
 
          return (timeList[obj1] || 0) - (timeList[obj2] || 0);
       });
-      data.links.forEach(function (objectHandle, index) {
+      list.forEach(function (objectHandle, index) {
 
          var state = dmz.object.state(objectHandle, dmz.mind.MindState)
-           , position = dmz.object.position(objectHandle, attr)
+           , position = dmz.object.position(objectHandle, dmz.mind.MindPosition)
            , delta
            ;
 
@@ -169,7 +155,7 @@ updateVoteObjectPositions = function (voteHandle, attr) {
             delta = position.subtract(data.lastPos);
          }
          else { delta = getPosition(index, data.links.length); }
-         if (delta) { dmz.object.position(objectHandle, attr, delta.add(data.position)); }
+         if (delta) { dmz.object.position(objectHandle, dmz.mind.MindPosition, delta.add(data.position)); }
       });
    }
 }
@@ -179,8 +165,8 @@ dmz.object.position.observe(self, dmz.mind.MindPosition, function (handle, attr,
    if (voteList[handle]) {
 
       voteList[handle].position = value;
-      voteList[handle].lastPos = prev;
-      updateVoteObjectPositions(handle, attr);
+      voteList[handle].lastPos = prev || value;
+      updateVoteObjectPositions(handle);
    }
 });
 
