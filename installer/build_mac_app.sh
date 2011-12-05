d5c62a92bd75c50aae97bfbcf127a685bbf627fa
Benjamin Sangster <benjamin.e.sangster@gmail.com>
2011-12-05 09:18:34 -0800

    Captialization fix for update.

--- installer/build_mac_app.sh
+++ installer/build_mac_app.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 DEPTH=../../..
 lmk -m opt -b
-cp -RL $DEPTH/bin/macos-opt/MINDMAP.app $DEPTH
-mkdir $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-mkdir $DEPTH/MINDMAP.app/Contents/Frameworks/Qt/plugins
-mkdir $DEPTH/MINDMAP.app/Contents/Frameworks/Qt/plugins/imageformats
-mkdir $DEPTH/MINDMAP.app/Contents/Frameworks/v8/
-mkdir $DEPTH/MINDMAP.app/Contents/Frameworks/Qt/plugins/phonon_backend
-cp $DEPTH/depend/Qt/QtCore $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-cp $DEPTH/depend/Qt/QtGui $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-cp $DEPTH/depend/Qt/QtXml $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-cp $DEPTH/depend/Qt/QtSvg $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-cp $DEPTH/depend/Qt/QtOpenGL $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-cp $DEPTH/depend/Qt/QtWebKit $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-cp $DEPTH/depend/Qt/QtNetwork $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-cp $DEPTH/depend/Qt/phonon $DEPTH/MINDMAP.app/Contents/Frameworks/Qt
-cp $DEPTH/depend/Qt/imageformats/libqgif.dylib $DEPTH/MINDMAP.app/Contents/Frameworks/Qt/plugins/imageformats
-cp $DEPTH/depend/Qt/imageformats/libqjpeg.dylib $DEPTH/MINDMAP.app/Contents/Frameworks/Qt/plugins/imageformats
-cp $DEPTH/depend/Qt/imageformats/libqtiff.dylib $DEPTH/MINDMAP.app/Contents/Frameworks/Qt/plugins/imageformats
-cp $DEPTH/depend/Qt/imageformats/libqsvg.dylib $DEPTH/MINDMAP.app/Contents/Frameworks/Qt/plugins/imageformats
-cp $DEPTH/depend/Qt/phonon_backend/libphonon_qt7.dylib $DEPTH/MINDMAP.app/Contents/Frameworks/Qt/plugins/phonon_backend/
+cp -RL $DEPTH/bin/macos-opt/mindmap.app $DEPTH
+mkdir $DEPTH/mindmap.app/Contents/Frameworks/Qt
+mkdir $DEPTH/mindmap.app/Contents/Frameworks/Qt/plugins
+mkdir $DEPTH/mindmap.app/Contents/Frameworks/Qt/plugins/imageformats
+mkdir $DEPTH/mindmap.app/Contents/Frameworks/v8/
+mkdir $DEPTH/mindmap.app/Contents/Frameworks/Qt/plugins/phonon_backend
+cp $DEPTH/depend/Qt/QtCore $DEPTH/mindmap.app/Contents/Frameworks/Qt
+cp $DEPTH/depend/Qt/QtGui $DEPTH/mindmap.app/Contents/Frameworks/Qt
+cp $DEPTH/depend/Qt/QtXml $DEPTH/mindmap.app/Contents/Frameworks/Qt
+cp $DEPTH/depend/Qt/QtSvg $DEPTH/mindmap.app/Contents/Frameworks/Qt
+cp $DEPTH/depend/Qt/QtOpenGL $DEPTH/mindmap.app/Contents/Frameworks/Qt
+cp $DEPTH/depend/Qt/QtWebKit $DEPTH/mindmap.app/Contents/Frameworks/Qt
+cp $DEPTH/depend/Qt/QtNetwork $DEPTH/mindmap.app/Contents/Frameworks/Qt
+cp $DEPTH/depend/Qt/phonon $DEPTH/mindmap.app/Contents/Frameworks/Qt
+cp $DEPTH/depend/Qt/imageformats/libqgif.dylib $DEPTH/mindmap.app/Contents/Frameworks/Qt/plugins/imageformats
+cp $DEPTH/depend/Qt/imageformats/libqjpeg.dylib $DEPTH/mindmap.app/Contents/Frameworks/Qt/plugins/imageformats
+cp $DEPTH/depend/Qt/imageformats/libqtiff.dylib $DEPTH/mindmap.app/Contents/Frameworks/Qt/plugins/imageformats
+cp $DEPTH/depend/Qt/imageformats/libqsvg.dylib $DEPTH/mindmap.app/Contents/Frameworks/Qt/plugins/imageformats
+cp $DEPTH/depend/Qt/phonon_backend/libphonon_qt7.dylib $DEPTH/mindmap.app/Contents/Frameworks/Qt/plugins/phonon_backend/
 if [ -d $DEPTH/depend/QtGui.framework/Versions/4/Resources/qt_menu.nib ] ; then
-cp -R $DEPTH/depend/QtGui.framework/Versions/4/Resources/qt_menu.nib $DEPTH/MINDMAP.app/Contents/Resources
+cp -R $DEPTH/depend/QtGui.framework/Versions/4/Resources/qt_menu.nib $DEPTH/mindmap.app/Contents/Resources
 fi
-cp $DEPTH/depend/v8/lib/libv8.dylib $DEPTH/MINDMAP.app/Contents/Frameworks/v8/
-mkdir $DEPTH/MINDMAP
-mv $DEPTH/MINDMAP.app $DEPTH/MINDMAP/
-ln -s /Applications $DEPTH/MINDMAP/
-TARGET=$DEPTH/MINDMAP-`cat $DEPTH/tmp/macos-opt/mindmapapp/versionnumber.txt`-`cat $DEPTH/tmp/macos-opt/mindmapapp/buildnumber.txt`.dmg
-hdiutil create -srcfolder $DEPTH/MINDMAP/ $TARGET
+cp $DEPTH/depend/v8/lib/libv8.dylib $DEPTH/mindmap.app/Contents/Frameworks/v8/
+mkdir $DEPTH/mindmap
+mv $DEPTH/mindmap.app $DEPTH/mindmap/
+ln -s /Applications $DEPTH/mindmap/
+TARGET=$DEPTH/mindmap-`cat $DEPTH/tmp/macos-opt/mindmapapp/versionnumber.txt`-`cat $DEPTH/tmp/macos-opt/mindmapapp/buildnumber.txt`.dmg
+hdiutil create -srcfolder $DEPTH/mindmap/ $TARGET
 hdiutil internet-enable -yes -verbose $TARGET
-rm -rf $DEPTH/MINDMAP/
+rm -rf $DEPTH/mindmap/
 INSTALLER_PATH=$DEPTH/installers
 if [ ! -d $INSTALLER_PATH ] ; then
    mkdir $INSTALLER_PATH
--- installer/publish_mac_app.sh
+++ installer/publish_mac_app.sh
@@ -8,15 +8,15 @@ if [ "$CHANNEL" = "" ] ; then
    CHANNEL=devel
 fi
 
-VERSION_XML=$DEPTH/bin/macos-opt/MINDMAP.app/Contents/Resources/config/version.xml
-UPDATE=MINDMAP-`cat $DEPTH/tmp/macos-opt/mindmapapp/versionnumber.txt`-`cat $DEPTH/tmp/macos-opt/mindmapapp/buildnumber.txt`
-OLD_UPDATE=MINDMAP-`cat $DEPTH/tmp/macos-opt/mindmapapp/buildnumber.txt`
+VERSION_XML=$DEPTH/bin/macos-opt/mindmap.app/Contents/Resources/config/version.xml
+UPDATE=mindmap-`cat $DEPTH/tmp/macos-opt/mindmapapp/versionnumber.txt`-`cat $DEPTH/tmp/macos-opt/mindmapapp/buildnumber.txt`
+OLD_UPDATE=mindmap-`cat $DEPTH/tmp/macos-opt/mindmapapp/buildnumber.txt`
 INSTALLER=$DEPTH/installers/$UPDATE.dmg
 
 echo "publishing $INSTALLER..."
 
-echo "scp $VERSION_XML dmzupdate.chds.us:/home/dmzupdate.chds.us/public/latest/macos-$CHANNEL/MINDMAP.xml"
-scp $VERSION_XML dmzupdate.chds.us:/home/dmzupdate.chds.us/public/latest/macos-$CHANNEL/MINDMAP.xml
+echo "scp $VERSION_XML dmzupdate.chds.us:/home/dmzupdate.chds.us/public/latest/macos-$CHANNEL/mindmap.xml"
+scp $VERSION_XML dmzupdate.chds.us:/home/dmzupdate.chds.us/public/latest/macos-$CHANNEL/mindmap.xml
 
 echo "scp ./changelog.html dmzupdate.chds.us:/home/dmzupdate.chds.us/public/downloads/$UPDATE.html"
 scp ./changelog.html dmzupdate.chds.us:/home/dmzupdate.chds.us/public/downloads/$UPDATE.html
